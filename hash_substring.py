e0348816aa488b069a99234f6f7150d6f069672d
reinards9 <125828462+reinards9@users.noreply.github.com>
2023-04-13 18:03:49 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,28 +1,39 @@
-def rabin_karp(text, pattern):
-    t_len = len(text)
-    p_len = len(pattern)
-    p_hash = hash(pattern)
-    t_hash = hash(text[:p_len])
-    positions = []
-
-    for i in range(t_len - p_len + 1):
-        if p_hash == t_hash and pattern == text[i:i + p_len]:
-            positions.append(str(i))
-
-        if i < t_len - p_len:
-            t_hash = hash(text[i + 1:i + p_len + 1])
-
-    return " ".join(positions)
+def read_input():
+   
+    input_type = input().rstrip()
+    if input_type == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == 'F':
+        with open('input.txt', 'r') as f:
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
+    else:
+        raise ValueError('Invalid input type')
+    return pattern, text
 
 
-def main():
-    pattern = input("Enter the pattern: ").strip()
-    text = input("Enter the text: ").strip()
-    positions = rabin_karp(text, pattern)
-    print("Occurrences of the pattern at positions:", positions)
+def print_occurrences(output):
+    
+    print(' '.join(map(str, output)))
+
+
+def get_occurrences(pattern, text):
+
+    p = len(pattern)
+    t = len(text)
+    p_hash = sum(ord(pattern[i]) * pow(10, p - i - 1) for i in range(p))
+    t_hash = sum(ord(text[i]) * pow(10, p - i - 1) for i in range(p))
+    occurrences = []
+    for i in range(t - p + 1):
+        if p_hash == t_hash:
+            if pattern == text[i:i+p]:
+                occurrences.append(i)
+        if i < t - p:
+            t_hash = (t_hash - ord(text[i]) * pow(10, p - 1)) * 10 + ord(text[i+p])
+    return occurrences
 
 
 if __name__ == '__main__':
-    main()
-
+    print_occurrences(get_occurrences(*read_input()))
 
