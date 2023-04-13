8d9820b9b69cf3f1ec9e3082b8bd383a917b1e28
reinards9 <125828462+reinards9@users.noreply.github.com>
2023-04-13 17:57:49 +0300

    Update main-8.py

--- main-8.py
+++ main-8.py
@@ -1,37 +1,17 @@
-import sys
-
-def hash(s):
-    p = 10**9 + 7
-    x = 263
-    ans = 0
-    for c in reversed(s):
-        ans = (ans * x + ord(c)) % p
-    return ans
-
 def rabin_karp(pattern, text):
     p_len = len(pattern)
     t_len = len(text)
     p_hash = hash(pattern)
     t_hashes = [hash(text[i:i+p_len]) for i in range(t_len - p_len + 1)]
-    indices = [i for i, h in enumerate(t_hashes) if h == p_hash]
+    indices = [i for i, h in enumerate(t_hashes) if h == p_hash and text[i:i+p_len] == pattern]
     return indices
 
 if __name__ == '__main__':
-    input_type = input("Enter input type (I for keyboard, F for file): ")
-    if input_type == "I":
-        pattern = input("Enter the pattern to search for: ")
-        text = input("Enter the text to search in: ")
-    elif input_type == "F":
-        with open("test.txt") as f:
-            pattern = f.readline().strip()
-            text = f.readline().strip()
-    else:
-        print("Invalid input type!")
-        sys.exit(1)
+    pattern = input().strip()
+    text = input().strip()
     indices = rabin_karp(pattern, text)
     for index in indices:
-        print(index, end=" ")
-    print()
+        print(index, end=' ')
 
 
 
