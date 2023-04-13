da5400f272addffa35889786e9fe4281c3fb59bf
reinards9 <125828462+reinards9@users.noreply.github.com>
2023-04-13 18:10:28 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,34 +1,32 @@
 def read_input():
-    # this function needs to acquire input from keyboard
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    return input().rstrip(), input().rstrip()
-
-def print_occurrences(output):
-    # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
+    input_type = input().rstrip()
+    if input_type == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == 'F':
+        with open('input.txt', 'r') as f:
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
+    else:
+        raise ValueError('Invalid input type')
+    return pattern, text
 
 def get_occurrences(pattern, text):
-    # this function should find the occurrences using Rabin Karp algorithm
-    p = 1000000007
-    x = 1
-    result = []
-    pattern_hash = poly_hash(pattern, p, x)
-    n = len(text)
-    m = len(pattern)
-    h = [0] * (n - m + 1)
-    s = text[n-m:]
-    h[n-m] = poly_hash(s, p, x)
-    y = pow(x, m, p)
-    for i in range(n-m-1, -1, -1):
-        h[i] = (x * h[i+1] + ord(text[i]) - y * ord(text[i+m])) % p
-    for i in range(n - m + 1):
-        if pattern_hash != h[i]:
-            continue
-        if text[i:i+m] == pattern:
-            result.append(i)
-    return result
+    p = len(pattern)
+    t = len(text)
+    p_hash = sum(ord(pattern[i]) * pow(10, p - i - 1) for i in range(p))
+    t_hash = sum(ord(text[i]) * pow(10, p - i - 1) for i in range(p))
+    occurrences = []
+    for i in range(t - p + 1):
+        if p_hash == t_hash:
+            if pattern == text[i:i+p]:
+                occurrences.append(i)
+        if i < t - p:
+            t_hash = (t_hash - ord(text[i]) * pow(10, p - 1)) * 10 + ord(text[i+p])
+    return occurrences
+
+def print_occurrences(output):
+    print(' '.join(map(str, output)))
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
