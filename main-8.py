c817bd9d11edd0defa6e871063b496fc37f23a25
reinards9 <125828462+reinards9@users.noreply.github.com>
2023-04-13 17:47:20 +0300

    Update main-8.py

--- main-8.py
+++ main-8.py
@@ -1,36 +1,28 @@
-def rabin_karp( text, pattern):
-    t_len = len(  text)
+import sys
 
-  
-    p_len = len( pattern )
-  
-    p_hash = hash(pattern )
-  
-    t_hash = hash(text[:p_len])
-  
-    positions = []
+def rabin_karp(pattern, text):
+    p_len = len(pattern)
+    t_len = len(text)
+    p_hash = hash(pattern)
+    t_hashes = [hash(text[i:i+p_len]) for i in range(t_len - p_len + 1)]
+    indices = [i for i, h in enumerate(t_hashes) if h == p_hash]
+    return indices
 
-    for i in range(t_len - p_len + 1):
-      
-        if p_hash == t_hash and pattern == text[i:i+p_len]:
-          
-            positions.append(str(i))
-
-        if i < t_len - p_len:
-            t_hash = hash(text[i+1:i+p_len+1])
-
-    return " ".join(positions)
-
-
-
-pattern = input().strip()
-
-text = input().strip()
-
-
-positions = rabin_karp(text, pattern)
-
-
-print(positions)
+if __name__ == '__main__':
+    input_type = input("Enter input type (I for keyboard, F for file): ")
+    if input_type == "I":
+        pattern = input("Enter the pattern to search for: ")
+        text = input("Enter the text to search in: ")
+    elif input_type == "F":
+        with open("test.txt") as f:
+            pattern = f.readline().strip()
+            text = f.readline().strip()
+    else:
+        print("Invalid input type!")
+        sys.exit(1)
+    indices = rabin_karp(pattern, text)
+    for index in indices:
+        print(index, end=" ")
+    print()
 
 
